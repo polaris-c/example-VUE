d7328f53df080ac51a2af1a1ca56dac31c3ae560
ZSY <1169846461@qq.com>
2017-11-28 16:36:57 +0800

    zsy master edit index2

--- index2.js
+++ index2.js
@@ -1,9 +1,89 @@
-var app7 = new Vue({
-	el : "#app7",
-	data: {
 
+// Vue.component('alert', {
+// 	template: '<button @click = "onClick" value = "TEST1">test1</button>',
+// 	methods: {
+// 		onClick: function() {
+// 			alert('Test1');
+// 		}
+// 	}
+// });
+
+var alertComponent = {
+	template: '<button :class = "btnClass" @click = "onClick" value = "TEST1">test1</button>',
+	data: function() {
+		return {
+			btnClass: 'btn btn-default'
+		}
 	},
 	methods: {
-	
+		onClick: function() {
+			console.log('Test1');
+		}
+	}
+};
+
+new Vue({
+	el : "#app9",
+    components: { 
+    	alert: alertComponent
+    },		
+	data: { 
+		btnClass: 'btn btn-default' // Useless!!!
+	},
+	methods: { }
+});
+
+/*************************************************************************
+ *
+ */
+// Vue.component('like', {
+// 	template: '#likeComponentTPL',
+// 	data: function(){
+// 		return {
+// 			likeCount: 10,
+// 			liked: false
+// 		}
+// 	},
+// 	methods: {
+// 		toggleLike: function() {
+// 			if(!this.liked) {
+// 				this.likeCount ++;
+// 			}
+// 			else {
+// 				this.likeCount --;
+// 			}
+// 			this.liked = !this.liked; 
+// 		}
+// 	}
+// });
+
+var likeComponent = {
+	template: '#likeComponentTPL',
+	data: function(){
+		return {
+			likeCount: 10,
+			liked: false,
+			btnClass: 'btn btn-default'
+		}
+	},
+	methods: {
+		toggleLike: function() {
+			if(!this.liked) {
+				this.likeCount ++;
+			}
+			else {
+				this.likeCount --;
+			}
+			this.liked = !this.liked; 
+		}
+	}
+};
+
+new Vue({
+	el: '#app10',
+    components: { 
+    	like: likeComponent
+    },	
+	data: {
 	}
 });
