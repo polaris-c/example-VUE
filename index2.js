05fb6322f48ae88e9ea62d151877006e7911d04f
ZSY <1169846461@qq.com>
2017-11-28 17:39:41 +0800

    zsy master add index2 app11

--- index2.js
+++ index2.js
@@ -1,4 +1,4 @@
-
+	
 // Vue.component('alert', {
 // 	template: '<button @click = "onClick" value = "TEST1">test1</button>',
 // 	methods: {
@@ -86,4 +86,45 @@ new Vue({
     },	
 	data: {
 	}
+});
+
+/*************************************************************************
+ *
+ */
+var alertComponent = {
+	template: '<button :class = "btnClass" @click = "onClick" value = "TEST2">test2</button>',
+	props: ['msg'],
+	data: function() {
+		return {
+			btnClass: 'btn btn-default'
+		}
+	},
+	methods: {
+		onClick: function() {
+			console.log(this.msg + '  alert');
+		}
+	}
+};
+
+var userComponent = {
+	template: '<button :class = "btnClass"><a :href = "\'/user/\' + username">@ {{ username }}</a></button>',
+	props: ['username'],
+	data: function() {
+		return {
+			btnClass: 'btn btn-default'
+		}
+	},
+	methods: { }
+};
+
+new Vue({
+	el : "#app11",
+    components: { 
+    	alert: alertComponent,
+    	user: userComponent
+    },		
+	data: { 
+		btnClass: 'btn btn-default' // Useless!!!
+	},
+	methods: { }
 });
