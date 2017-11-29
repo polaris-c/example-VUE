ebe97ddeea9d2742dd2c31efd7bcf1e536969f16
ZSY <1169846461@qq.com>
2017-11-29 16:16:30 +0800

    zsy master annotation index2 app11

--- index2.js
+++ index2.js
@@ -2,7 +2,7 @@
  * 组件 全局与局部  
  * Vue.component自定义标签需要放置在new Vue({ })定义的域中，即el:'#id'标签中
  * new Vue({ }) components中的组件是局部组件
- * 组件的data中定义的class可以通过v-bind在模板中起作用，new Vue({ })的data中定义的class对组件无效
+ * 组件component的data中定义的class可以通过v-bind在模板template中起作用，new Vue({ })的data中定义的class对组件无效
  * Vue.component('name', { }); 可以写成 var nameComponent = { }; 的形式
  */
 
@@ -44,6 +44,7 @@ new Vue({
  * 配置组件
  * template:'#id'是将组件模板放置在html页面中
  */
+
 // Vue.component('like', {
 // 	template: '#likeComponentTPL',
 // 	data: function(){
@@ -100,14 +101,31 @@ new Vue({
 /*************************************************************************
  * 父->子 组件通信
  * <alert> --msg--> <button>
- * <user>  --username--> <button>
+ * <user>  --username -> <button>
+ * template中的表单可以通过v-model绑定值，其值定义在组件component的data中，注意不是任何元素都能用v-model
+ * template中也可以直接用{{ }}绑定值，其值也定义在组件component的data中
+ * 报错：Component template should contain exactly one root element. 组件模板应该只包含一个根元素
+ * 父组件中v-bind绑定的属性可以通过new Vue({ })中的data确定值，并传给子组件
  */
+
 var alertComponent = {
-	template: '<button :class = "btnClass" @click = "onClick" value = "TEST2">test2</button>',
+	template: `
+    <div>
+	    <button :class = "btnClass" @click = "onClick"  value = "TEST2">TEST2</button> 
+	    <br>
+	    <input type="text" v-model = "test01">
+	    <br>
+	    input v-model绑定: {{ test01 }}
+	    <br>
+	    <div> div 直接绑定: {{ test02 }}</div>
+    </div>
+    `,
 	props: ['msg'],
 	data: function() {
 		return {
-			btnClass: 'btn btn-default'
+			btnClass: 'btn btn-default',
+			test01: 'input v-model = "test01"',    // template中的表单通过v-model绑定的值
+			test02: '<div> => { {test02} }'
 		}
 	},
 	methods: {
@@ -122,7 +140,8 @@ var userComponent = {
 	props: ['username'],
 	data: function() {
 		return {
-			btnClass: 'btn btn-default'
+			btnClass: 'btn btn-default',
+			ZSY: 'v-bind:username = "ZSY"'    // Useless!!!
 		}
 	},
 	methods: { }
@@ -135,14 +154,17 @@ new Vue({
     	user: userComponent
     },		
 	data: { 
-		btnClass: 'btn btn-default' // Useless!!!
+		btnClass: 'btn btn-default',    // Useless!!!
+		ZSY: 'v-bind:username="ZSY"'    // 父组件中v-bind绑定的属性值
 	},
 	methods: { }
 });
 
 /*************************************************************************
  * 子->父 组件通信
+ * <balance> <--$emit showBalance-- <show>
  */
+
 Vue.component('balance',{
  	template:`
  	<div>
@@ -159,7 +181,7 @@ Vue.component('balance',{
     methods:{
     	showBalanceFun: function(data){
     		this.showBalanceFlag = true;
-    		console.log('data a:', data.a, '***', 'data b:', data.b);
+    		console.log('data.a:', data.a, '***', 'data.b:', data.b);
     	}
     } 	
 });
@@ -168,7 +190,7 @@ Vue.component('show',{
     template:'<button @click = "onClick()">显示余额</button>',
     methods:{
     	onClick: function(){
-    		this.$emit('showBalance', {a: 1, b: 2});  //触发自定义事件showBalance
+    		this.$emit('showBalance', {a: 1, b: 2});    //触发自定义事件showBalance
     	}
     }
 });
@@ -176,3 +198,8 @@ Vue.component('show',{
 new Vue({
 	el:'#app12',
 });
+
+/*************************************************************************
+ *  组件通信
+ * <> <-- -- <>
+ */
